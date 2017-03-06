d741ff4218828be7843d34dcac92d7691ca9a0fd
Rui Freitas <dinnerpartysaboteur@gmail.com>
2017-03-06 13:02:56 +0000

    date navigation and save

--- app/models/routine.rb
+++ app/models/routine.rb
@@ -10,6 +10,26 @@ class Routine < ApplicationRecord
     self.answers.where(created_at: Time.now.beginning_of_day.utc..Time.now.end_of_day.utc).first_or_create!
   end
 
+  def previous_day(date)
+    answer = self.answers.where(created_at: (date - 1.day).beginning_of_day.utc..(date - 1.day).end_of_day.utc).first_or_create!
+    answer.created_at = date - 1.day
+    answer.save
+    self.questions.each do |question|
+        field = answer.new_question(question) unless answer.exists?(question)
+    end
+    return answer
+  end
+
+  def next_day(date)
+    answer = self.answers.where(created_at: (date + 1.day).beginning_of_day.utc..(date + 1.day).end_of_day.utc).first_or_create!
+    answer.created_at = date + 1.day
+    answer.save
+    self.questions.each do |question|
+        field = answer.new_question(question) unless answer.exists?(question)
+    end
+    return answer
+  end
+
   def exists?(question)
     results = self.answers.joins(:fields).where(fields: { question_id: question.id })
     results.empty? ? false : true
