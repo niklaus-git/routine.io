e47a9e9b45378728d60e6fa0b07b33b0a66ba47a
Rui Freitas <dinnerpartysaboteur@gmail.com>
2017-03-09 19:43:25 +0000

    number input js validation

--- app/assets/javascripts/answers.js
+++ app/assets/javascripts/answers.js
@@ -113,7 +113,7 @@ $('.choice').click(function() {
   };
 });
 
-$('.numbers input').keypress(function(e) {
+$('.numbers input').keydown(function(e) {
       if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 110, 190]) !== -1 ||
              // Allow: Ctrl+A, Command+A
             (e.keyCode === 65 && (e.ctrlKey === true || e.metaKey === true)) ||
@@ -124,7 +124,7 @@ $('.numbers input').keypress(function(e) {
                  return;
         }
         // Ensure that it is a number and stop the keypress
-        if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) {
+        if ((e.which < 48 || e.keyCode > 57) && (e.keyCode < 96 || e.keyCode > 105)) {
             e.preventDefault();
             $(this).closest('li').effect('shake');
         }
